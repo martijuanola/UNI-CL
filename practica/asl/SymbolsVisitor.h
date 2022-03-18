49a01fec0400cc70705dfcb8bf00dd8f51eefe8b
Gil Ralló <gil.rallo01@gmail.com>
2022-03-18 12:00:28 +0100

    Update SymbolsVisitor.h

--- practica/asl/SymbolsVisitor.h
+++ practica/asl/SymbolsVisitor.h
@@ -68,9 +68,10 @@ public:
   antlrcpp::Any visitArray_decl(AslParser::Array_declContext *ctx);
   antlrcpp::Any visitType(AslParser::TypeContext *ctx);
   antlrcpp::Any visitBasic_type(AslParser::Basic_typeContext *ctx);
-   antlrcpp::Any visitStatements(AslParser::StatementsContext *ctx);
-   antlrcpp::Any visitAssignStmt(AslParser::AssignStmtContext *ctx);
-  // antlrcpp::Any visitIfStmt(AslParser::IfStmtContext *ctx);
+  antlrcpp::Any visitStatements(AslParser::StatementsContext *ctx);
+  antlrcpp::Any visitAssignStmt(AslParser::AssignStmtContext *ctx);
+  antlrcpp::Any visitIfStmt(AslParser::IfStmtContext *ctx);
+  antlrcpp::Any visitWhileStmt(AslParser::WhileStmtContext *ctx);
   // antlrcpp::Any visitProcCall(AslParser::ProcCallContext *ctx);
   // antlrcpp::Any visitReadStmt(AslParser::ReadStmtContext *ctx);
   // antlrcpp::Any visitWriteExpr(AslParser::WriteExprContext *ctx);
