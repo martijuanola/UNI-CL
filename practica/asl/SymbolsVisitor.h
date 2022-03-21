e95457d3f4409ee71b2db5633051f602d905342d
Gil Ralló <gil.rallo01@gmail.com>
2022-03-22 00:00:55 +0100

    Update SymbolsVisitor.h

--- practica/asl/SymbolsVisitor.h
+++ practica/asl/SymbolsVisitor.h
@@ -72,16 +72,18 @@ public:
   antlrcpp::Any visitAssignStmt(AslParser::AssignStmtContext *ctx);
   antlrcpp::Any visitIfStmt(AslParser::IfStmtContext *ctx);
   antlrcpp::Any visitWhileStmt(AslParser::WhileStmtContext *ctx);
-  // antlrcpp::Any visitProcCall(AslParser::ProcCallContext *ctx);
-  // antlrcpp::Any visitReadStmt(AslParser::ReadStmtContext *ctx);
-  // antlrcpp::Any visitWriteExpr(AslParser::WriteExprContext *ctx);
-  // antlrcpp::Any visitWriteString(AslParser::WriteStringContext *ctx);
-  // antlrcpp::Any visitLeft_expr(AslParser::Left_exprContext *ctx);
-  // antlrcpp::Any visitExprIdent(AslParser::ExprIdentContext *ctx);
-  // antlrcpp::Any visitArithmetic(AslParser::ArithmeticContext *ctx);
-  // antlrcpp::Any visitRelational(AslParser::RelationalContext *ctx);
-  // antlrcpp::Any visitValue(AslParser::ValueContext *ctx);
-  // antlrcpp::Any visitIdent(AslParser::IdentContext *ctx);
+  antlrcpp::Any visitProcCall(AslParser::ProcCallContext *ctx);
+  antlrcpp::Any visitReadStmt(AslParser::ReadStmtContext *ctx);
+  antlrcpp::Any visitWriteExpr(AslParser::WriteExprContext *ctx);
+  antlrcpp::Any visitWriteString(AslParser::WriteStringContext *ctx);
+  antlrcpp::Any visitLeft_expr(AslParser::Left_exprContext *ctx);
+  antlrcpp::Any visitExprIdent(AslParser::ExprIdentContext *ctx);   // aqui tmb comentat
+  antlrcpp::Any visitUnary(AslParser::UnaryContext *ctx);
+  antlrcpp::Any visitArithmetic(AslParser::ArithmeticContext *ctx);
+  antlrcpp::Any visitRelational(AslParser::RelationalContext *ctx);
+  antlrcpp::Any visitBoolean(AslParser::BooleanContext *ctx);
+  antlrcpp::Any visitValue(AslParser::ValueContext *ctx);
+  antlrcpp::Any visitIdent(AslParser::IdentContext *ctx);
 
 private:
 
