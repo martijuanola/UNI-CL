cf5b7e1218ed6063086a23e693e18602a62062cc
Gil Ralló <gil.rallo01@gmail.com>
2022-04-19 10:22:37 +0200

    Update TypeCheckVisitor.h

--- practica/asl/TypeCheckVisitor.h
+++ practica/asl/TypeCheckVisitor.h
@@ -77,6 +77,7 @@ public:
   antlrcpp::Any visitWriteString(AslParser::WriteStringContext *ctx);
   antlrcpp::Any visitLeft_expr(AslParser::Left_exprContext *ctx);
   antlrcpp::Any visitExprIdent(AslParser::ExprIdentContext *ctx);
+  antlrcpp::Any visitParenthesis(AslParser::ParenthesisContext *ctx);
   antlrcpp::Any visitUnary(AslParser::UnaryContext *ctx);
   antlrcpp::Any visitArithmetic(AslParser::ArithmeticContext *ctx);
   antlrcpp::Any visitRelational(AslParser::RelationalContext *ctx);
